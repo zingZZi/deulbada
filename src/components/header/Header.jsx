8cc086abd6fbd89e8cc2546e0fbb641049f75a73
zingzzi <pangdoa.merry@gmail.com>
2025-08-01 00:58:56 +0900

    Add: 기본 header 레이아웃 작업

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,22 @@
-import { StyledHeader } from './Header.style';
+import * as Styled from './Header.style';
+import Logo from './../../assets/images/deulbadaLogo.svg';
+import { Link } from 'react-router-dom';
+import { SearchIcon } from '../icon/Icons';
 
 const Header = () => {
-  return <StyledHeader>헤더영역</StyledHeader>;
+  return (
+    <Styled.Header>
+      <Styled.Logo>
+        <Link to="/home">
+          <img src={Logo} alt="" />
+        </Link>
+      </Styled.Logo>
+
+      <Link to="/search">
+        <span className="text-ir">검색창</span>
+        <SearchIcon size={'2.4rem'} />
+      </Link>
+    </Styled.Header>
+  );
 };
 export default Header;
--- src/components/header/Header.style.js
+++ src/components/header/Header.style.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
-export const StyledHeader = styled.header`
-  padding: 0px 1.6rem;
-  min-height: 48px;
+export const Header = styled.header`
+  padding: 0.8rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Logo = styled.h1`
+  font-size: 0;
+  width: 3.2rem;
+  height: 3.2rem;
+  img {
+    width: 100%;
+  }
 `;
