a94e55cd3eac0085eb0bd0b7c5cb8f8058178d2e
parkdasom0214 <parkdasom0214@gmail.com>
2025-08-07 22:40:03 +0900

    Add: JoinMembership 이메일, 비밀번호, 비밀번호 확인 유효성 검사 기능 추가

--- src/pages/join-membership/JoinMembership.jsx
+++ src/pages/join-membership/JoinMembership.jsx
@@ -1,25 +1,83 @@
 import * as Styled from './JoinMembership.style';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+
 const JoinMembership = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rePassword, setRePassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newErrors = {};
+
+    if (!email.includes('@')) {
+  newErrors.email = '이메일 형식이 올바르지 않습니다.';
+}
+
+    if (password.length < 8) {
+      newErrors.password = '비밀번호는 8자 이상이어야 합니다.';
+    }
+
+    if (password !== rePassword) {
+      newErrors.rePassword = '비밀번호가 일치하지 않습니다.';
+    }
+
+    setErrors(newErrors);
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    newErrors.email = '올바른 이메일 주소를 입력해주세요.';
+  }
+
+    if (Object.keys(newErrors).length === 0) {
+      // 유효성 통과 → 다음 단계로 진행
+      alert('회원가입 가능!');
+    }
+  };
+
+
   return (
-  <Styled.Form>
+  <Styled.Form onSubmit={handleSubmit}>
 
     <Styled.H2>이메일로 회원가입</Styled.H2>
 
     <Styled.InputGroup>
       <Styled.Label htmlFor="email">이메일</Styled.Label>
-      <Styled.InputEmail id="email" type="email" placeholder="이메일 주소를 입력해주세요" />
+      <Styled.InputEmail
+        id="email"
+        type="text"
+        value={email}
+        placeholder="이메일 주소를 입력해주세요"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      {errors.email && <Styled.Error>{errors.email}</Styled.Error>}
     </Styled.InputGroup>
     
     <Styled.InputGroup>
       <Styled.Label htmlFor="password">비밀번호</Styled.Label>
-      <Styled.InputPassword id="password" type="password" placeholder="비밀번호를 설정해 주세요" />
+      <Styled.InputPassword 
+        id="password" 
+        type="password" 
+        placeholder="비밀번호를 설정해 주세요"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      {errors.password && <Styled.Error>{errors.password}</Styled.Error>}
     </Styled.InputGroup>
 
     <Styled.InputGroup>
-      <Styled.Label htmlFor="password">비밀번호 확인</Styled.Label>
-      <Styled.InputPassword id="re-password" type="password" placeholder="한 번 더 입력해주세요" />
+      <Styled.Label htmlFor="re-password">비밀번호 확인</Styled.Label>
+      <Styled.InputPassword
+        id="re-password" 
+        type="password" 
+        placeholder="한 번 더 입력해주세요"
+        value={rePassword}
+        onChange={(e) => setRePassword(e.target.value)}
+      />
+      {errors.rePassword && <Styled.Error>{errors.rePassword}</Styled.Error>}
     </Styled.InputGroup>
 
     <Styled.Button type="submit">다음</Styled.Button>
