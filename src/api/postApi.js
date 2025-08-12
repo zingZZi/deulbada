1bdfa71e74d712c7c8ecf7fee959d9144007b6dd
zingzzi <pangdoa.merry@gmail.com>
2025-08-12 16:17:19 +0900

    Edit: 에러 수정관련 커밋

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -6,6 +6,12 @@ export const fetchPosts = () => api.get('/posts');
 export const createPost = (postData) => api.post('/posts/', postData);
 // GET: 게시물 상세 조회
 export const getPostDetail = (id) => api.get(`/posts/${id}/`);
+// GET: user  postList조회
+export const getUserPost = (userId) => api.get(`/posts/${userId}/`);
+// Delete: user  post 삭제
+export const delePost = (postId) => api.delete(`/posts/${postId}/`);
 
-//좋아요수
-export const fetchLike = (id) => api.get(`/posts/${id}/like/`);
+//좋아요 상호작용
+export const togglePostLike = (id) => api.post(`/posts/${id}/like/`);
+//commonet 글쓰기
+export const fetchComment = (id) => api.post(`/posts/${id}/comment/`);
--- src/api/productApi.js
+++ src/api/productApi.js
@@ -3,6 +3,9 @@ import api from '../api/api';
 // GET: 전체 상품 조회
 export const fetchProduct = () => api.get('/products/');
 // GET: 카테고리별 상품조회
-export const getPostDetail = (id) => axiosInstance.get(`/products/${id}/`);
+export const fetchProductFilter = (category, page = 1, limit = 4) =>
+  api.get(`/products/${category}/?page=${page}&limit=${limit}`);
+// GET: 유저 상품조회
+export const getProductUser = (user_name) => api.get(`/products/${user_name}/`);
 // POST: 상품등록
 export const createPost = () => api.get('/products/');
